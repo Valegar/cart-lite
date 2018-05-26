c9e6b45eea7d9b2b7fc630c7fb9e032aac8dbe72
Valegar <valentin.carage@gmail.com>
2018-05-26 16:01:07 +0200

    Test products list

---
+++ tests/Controller/HomeControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeControllerTest extends WebTestCase
+{
+    public function testProductsList()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $this->assertSame(12, $crawler->filter('.product-item')->count());
+    }
+}
