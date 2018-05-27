a58af89cd272162159adbbfc7f1b1131ceedfa39
Valegar <valentin.carage@gmail.com>
2018-05-27 09:43:26 +0200

    Enable translation switch from the UI

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -9,7 +9,7 @@ class HomeControllerTest extends WebTestCase
     public function testProductsList()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler = $client->request('GET', '/en/');
 
         $this->assertSame(200, $client->getResponse()->getStatusCode());
         $this->assertSame(12, $crawler->filter('.product-item')->count());
